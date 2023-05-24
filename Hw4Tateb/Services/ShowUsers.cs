af778cbe408d97c5532cfa6e644d8df1b04f1e35
tayebmoradi <tayeb.mora69@gmail.com>
2023-05-24 19:04:32 +0330

    Add view in program

--- Hw4Tateb/Services/ShowUsers.cs
+++ Hw4Tateb/Services/ShowUsers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hw4Tateb.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace Hw4Tateb.Services
 {
-    internal class ShowUsers
+    public class ShowUsers
     {
+        UserRepository userRepository = new UserRepository();
+        public void ShowAllUsers()
+        {
+            var Users = userRepository.GetUsers();
+            foreach (var item in Users)
+            {
+                Console.WriteLine($"Id : {item.Id} , FullName : {item.FullName} , DateOfBirth : {item.DateOfBirth} , PhoneNumber : {item.PhoneNumber} , NationalCode : {item.NationalCode}");
+            }
+        }
     }
 }
---
+++ Hw4Tateb/View/Program.cs
@@ -0,0 +1,127 @@
+﻿using Hw4Tateb.Entity;
+using Hw4Tateb.Repository;
+using Hw4Tateb.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw4Tateb
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowUsers showUsers = new ShowUsers();
+            Random random = new Random();
+            UserRepository userRepository = new UserRepository();
+            User user = new User();
+
+            bool Flage = true;
+            while (Flage)
+            {
+                Console.WriteLine("Hello dear user");
+                Console.WriteLine("Select the desired option");
+                Menu.MenuUser();
+                Console.WriteLine("Enter the desired number");
+                string UserItem = Console.ReadLine();
+
+                if (int.TryParse(UserItem, out int id))
+                {
+                    if (UserItem == "1")
+                    {
+                        var Users = userRepository.GetUsers();
+                        var UserCount = Users.Count();
+                        var Count = Math.Max(0, UserCount + 1);
+
+                        Console.WriteLine("Enter your full name");
+                        string Name = Console.ReadLine();
+
+                        Console.WriteLine("Enter your PhoneNumber");
+                        string PhoneNumber = Console.ReadLine();
+
+                        Console.WriteLine("Enter your Dateofbirth");
+                        Console.WriteLine("sample Code : 5/24/2023 4:07:10 PM");
+                        string Dateofbirth = Console.ReadLine();
+
+                        if (PhoneNumber.Length <= 11)
+                        {
+                            user.Id = Count;
+                            user.PhoneNumber = PhoneNumber;
+                            user.DateOfBirth = Dateofbirth;
+                            user.FullName = Name;
+                            user.NationalCode = random.Next(100).ToString();
+                            user.CreatedDate = DateTime.Now.ToString();
+                            userRepository.AddUser(user);
+                            Console.WriteLine("User added successfully");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mobile number must be 11 digits");
+                        }
+                    }
+                    else if (UserItem == "2")
+                    {
+                        showUsers.ShowAllUsers();
+
+                        Console.WriteLine("Please enter the ID of the desired user");
+                        Console.WriteLine("Enter your Id");
+                        int Id = Convert.ToInt32(Console.ReadLine());
+
+
+                        Console.WriteLine("Enter your full name");
+                        string Name = Console.ReadLine();
+
+                        Console.WriteLine("Enter your PhoneNumber");
+                        string PhoneNumber = Console.ReadLine();
+
+                        Console.WriteLine("Enter your Dateofbirth");
+                        string Dateofbirth = Console.ReadLine();
+                        Console.WriteLine("Enter your NationalCode");
+                        string NationalCode = Console.ReadLine();
+                        User user1 = new User() { Id = Id, FullName = Name, NationalCode = NationalCode, PhoneNumber = PhoneNumber, DateOfBirth = Dateofbirth, CreatedDate = DateTime.Now.ToString() };
+                        userRepository.UpdateUser(user1);
+                    }
+                    else if (UserItem == "3")
+                    {
+                        var Users1 = userRepository.GetUsers();
+                        foreach (var item in Users1)
+                        {
+                            Console.WriteLine($"Id : {item.Id} , FullName : {item.FullName} , DateOfBirth : {item.DateOfBirth} , PhoneNumber : {item.PhoneNumber} , NationalCode : {item.NationalCode}");
+                        }
+                        Console.WriteLine("Please enter the ID of the desired user");
+                        Console.WriteLine("Enter your Id");
+                        int DeleteId = Convert.ToInt32(Console.ReadLine());
+                        User user2 = new User();
+                        foreach (var item in Users1)
+                        {
+                            if (item.Id == DeleteId)
+                            {
+                                user2 = item;
+                            }
+
+                        }
+                        userRepository.DeleteUser(user2);
+
+                    }
+                    else if (UserItem == "4")
+                    {
+                        showUsers.ShowAllUsers();
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number you selected does not exist in the menu");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
