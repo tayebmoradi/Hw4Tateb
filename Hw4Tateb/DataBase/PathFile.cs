fce5d3e99458abd3593debb8ae65e6a3994db973
tayebmoradi <tayeb.mora69@gmail.com>
2023-05-24 14:58:57 +0330

    add class Path file

---
+++ Hw4Tateb/DataBase/PathFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw4Tateb.DataBase
+{
+
+    public class PathFile
+    {
+        private string workingDirectory;
+        private string projectDirectory;
+        public string PathFileDataBase()
+        {
+            workingDirectory = Environment.CurrentDirectory;
+            projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+            return $"{projectDirectory}\\DataBase\\UsersCsvFile.csv";
+        }
+    }
+}
