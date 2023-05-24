a50ff0e3ad5b8cee1794b184c9ebde7af16b7cd7
tayebmoradi <tayeb.mora69@gmail.com>
2023-05-24 16:01:35 +0330

    Add Menu User

---
+++ Hw4Tateb/Services/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw4Tateb.Services
+{
+    public  class Menu
+    {
+        public static void MenuUser()
+        {
+
+            Console.WriteLine("1 = AddUser");
+            Console.WriteLine("2 = UpdateUser");
+            Console.WriteLine("3 = DeleteUser");
+            Console.WriteLine("4 = GetAllUser");
+
+        }
+    }
+}
